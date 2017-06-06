1a7a876032e62d751b04dd36047fc2c39b9d45f0
Jason Tam <JasonTam22@gmail.com>
2017-06-06 10:53:52 -0400

    some upfront conversion and use native defaultdict instead of pd df lookup (#12)

--- tiefrex/naive_sampler.py
+++ tiefrex/naive_sampler.py
@@ -2,6 +2,7 @@
 Implements a generator for basic uniform random sampling of negative items
 """
 import numpy as np
+from collections import defaultdict
 from tiefrex.constants import *
 
 
@@ -13,6 +14,14 @@ def batcher(iterable, n=1):
         yield iterable[ii:min(ii + n, l)]
 
 
+def feat_lookup(ids, feat_code_d):
+    ret = defaultdict(list)
+    for id_i in ids:
+        for k, v in feat_code_d[id_i].items():
+            ret[k].append(v)
+    return ret
+
+
 def feed_dicter(
         shuffle_inds, batch_size,
         interactions_df,
@@ -21,6 +30,10 @@ def feed_dicter(
         item_ids,
         input_pair_d,
 ):
+    # Upfront Conversion
+    user_feats_codes_d = user_feats_codes_df.to_dict(orient='index')
+    item_feats_codes_d = item_feats_codes_df.to_dict(orient='index')
+
     while True:
         np.random.shuffle(shuffle_inds)
         inds_batcher = batcher(shuffle_inds, n=batch_size)
@@ -31,12 +44,9 @@ def feed_dicter(
             # Uniform (does not even check that the neg is not a pos)
             neg_item_ids = np.random.choice(item_ids, batch_size)
 
-            user_feed_d = user_feats_codes_df.loc[user_ids].to_dict(
-                orient='list')
-            pos_item_feed_d = item_feats_codes_df.loc[pos_item_ids].to_dict(
-                orient='list')
-            neg_item_feed_d = item_feats_codes_df.loc[neg_item_ids].to_dict(
-                orient='list')
+            user_feed_d = feat_lookup(user_ids, user_feats_codes_d)
+            pos_item_feed_d = feat_lookup(pos_item_ids, item_feats_codes_d)
+            neg_item_feed_d = feat_lookup(neg_item_ids, item_feats_codes_d)
 
             feed_pair_dict = {
                 **{input_pair_d[f'{USER_VAR_TAG}.{feat_name}']: data_in
@@ -46,5 +56,5 @@ def feed_dicter(
                 **{input_pair_d[f'{NEG_VAR_TAG}.{feat_name}']: data_in
                    for feat_name, data_in in neg_item_feed_d.items()},
             }
-
             yield feed_pair_dict
+
