9937af32d3d8157655f8d37706a77408dc6fe626
Jason Tam <JasonTam22@gmail.com>
2018-05-18 12:20:35 -0400

    typo and comments

--- tophat/embedding.py
+++ tophat/embedding.py
@@ -52,11 +52,13 @@ class EmbeddingMap(object):
             vis_emb_user_col: If provided, give users an additional
                 visual-specific embedding (the value of the argument will 
                 denote the name of the user column)
-            init_emb_d: Optional dictionary of weights to initialize embeddings
+            init_emb_d: Optional dictionary of weights to initialize
+                embeddings. Keys are feature names and values are embedding
+                tensors.
                 Can be a subset of features, but needs to conform to the
                 proper shape of the embedding. If there are categories missing
                 from a feature's embedding initialization, please fill them in
-                prior with some initialization scheme. 
+                prior with some initialization scheme (see `inits_via_df`).
                 (Warning: this does not load in any saved gradients or whatever
                 weights are used by the optimizer -- so it's only halfway to
                 actually resuming a model)
